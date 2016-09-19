d6915ebea9f62a585664c0b69fce556869eb7948
Marius Zaleskis <zaleskis.m@gmail.com>
2016-09-19 23:25:14 +0200

    Connection made

---
+++ php-databases/inc/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+try {
+	$db = new PDO("mysql:host=localhost;dbname=database;port=3306","mariususer","murkis");
+	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}
+catch (Exception $e) {
+	echo "Unable to connect";
+	echo $e->getMessage();
+	exit;
+}
+
+echo "Connected successfuly";
+
+?>

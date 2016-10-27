3dbd99bbae9b9f415b3824b6b8df5dd1c6fa0358
Marius Zaleskis <zaleskis.m@gmail.com>
2016-10-27 17:16:45 +0300

    Bootstrap to Wordpress course

---
+++ wp-project/wp-content/themes/bootstrap-to-wp/sidebar-blog.php
@@ -0,0 +1,9 @@
+<div class="col-md-3 sidebar">
+
+	<?php if (! dynamic_sidebar('blog')) : ?>
+
+	<h3>Sidebar Setup</h3>
+    <p>No widget work done.</p>
+
+	<?php endif; ?>
+</div>
---
+++ wp-project/wp-content/themes/bootstrap-to-wp/sidebar.php
@@ -0,0 +1,9 @@
+<div class="col-md-3 sidebar sidebar-offcanvas">
+
+	<?php if (! dynamic_sidebar('page')) : ?>
+
+	<h3>Sidebar Setup</h3>
+    <p>No widget work done.</p>
+
+	<?php endif; ?>
+</div>

e17fe5e9c13ac5e620940df9c3fa138a7c560ead
Marius Zaleskis <zaleskis.m@gmail.com>
2016-11-02 09:24:26 +0200

    Woocommerce training

---
+++ wp-project/wp-content/themes/wootree/footer.php
@@ -0,0 +1,15 @@
+    <div class="footer-clear"></div>
+<footer class="row no-max pad">       
+	<div class="small-12 medium-6 columns">
+  		<p>Copyright <?php echo date('Y'); ?></p>
+  	</div>
+  	<div class="small-12 medium-6 columns right">
+  		<?php do_action('wpt_footer'); ?>
+  	</div>
+
+</footer>
+
+  <?php wp_footer(); ?>
+
+  </body>
+</html>

b123c838d38744cb44eec6a9ece922d86ca5e652
Marius Zaleskis <zaleskis.m@gmail.com>
2017-01-11 16:53:56 +0200

    Node js practise

---
+++ Javascript Full Stack/Build a Simple Dynamic Site with Node.js/app.js
@@ -0,0 +1,21 @@
+var router = require("./router.js");
+//Problem: We need a simple way to look at a user's badge count and JavaScript point from a web browser
+//Solution: Use Node.js to perform the profile look ups and server our template via HTTP
+
+//Create a web server
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+var http = require('http');
+http.createServer(function (request, response) {
+  router.home(request, response);
+  router.user(request, response);
+}).listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
+
